1664605af62ca2d4148259f019adc72b648347bd
DanUk2 <43887736+DanUk2@users.noreply.github.com>
2019-05-03 19:01:50 +0100

    Add files via upload
    I have created the fundamental game loops and functions needed to display the game and organized them for future use.

---
+++ main.py
@@ -0,0 +1,65 @@
+# platformer
+import pygame as pg
+import random
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        #create game window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("My Game")
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        #start new game
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        #game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        #game loop update
+        self.all_sprites.update()
+
+    def events(self):
+        #game loop events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        #game loop draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        #start screen
+        pass
+
+    def show_go_screen(self):
+        #game over screen
+        pass
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
