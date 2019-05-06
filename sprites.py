b94cb5a6d0fb773fbcf42f4e5b140218a8f9512d
DanUk2 <43887736+DanUk2@users.noreply.github.com>
2019-05-06 20:19:50 +0100

    Add files via upload
    Added projectiles and animations for shooting ability

--- sprites.py
+++ sprites.py
@@ -6,6 +6,15 @@ vec = pg.math.Vector2
 
 
 plat = pg.image.load('img/plat.png')
+bullet = pg.image.load('img/bullet.png')
+bg = pg.image.load('img/bg.png')
+
+
+left = False
+right = False
+
+
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
@@ -13,10 +22,13 @@ class Player(pg.sprite.Sprite):
         self.game = game
         self.walking = False
         self.jumping = False
+        self.shooting = False
+        self.left = False
+        self.right = False
         self.current_frame = 0
         self.last_update = 0
         self.load_images()
-        self.image = self.standing_frames[0]
+        self.image = self.standing_frames_r[0]
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
@@ -28,8 +40,22 @@ class Player(pg.sprite.Sprite):
         self.walk_frames_l = []
         for frame in self.walk_frames_r:
             self.walk_frames_l.append(pg.transform.flip(frame, True, False))
-        self.standing_frames = [pg.image.load('img/idle1.png'), pg.image.load('img/idle2.png'), pg.image.load('img/idle3.png'), pg.image.load('img/idle1.png'), pg.image.load('img/idle1.png')]
-        self.jumping_frames = [pg.image.load('img/jump1.png'), pg.image.load('img/jump2.png'), pg.image.load('img/jump3.png')]
+        self.standing_frames_r = [pg.image.load('img/shooting3.png')]
+        self.standing_frames_l = []
+        for frame in self.standing_frames_r:
+            self.standing_frames_l.append(pg.transform.flip(frame, True, False))
+        self.jumping_frames_r = [pg.image.load('img/jump1.png'), pg.image.load('img/jump2.png'), pg.image.load('img/jump3.png'), pg.image.load('img/jump3.png'), pg.image.load('img/jump3.png'), pg.image.load('img/jump3.png'), pg.image.load('img/jump3.png')]
+        self.jumping_frames_l = []
+        for frame in self.jumping_frames_r:
+            self.jumping_frames_l.append(pg.transform.flip(frame, True, False))
+        self.shooting_frames_r = [pg.image.load('img/runShoot1.png'), pg.image.load('img/runShoot2.png'), pg.image.load('img/runShoot3.png')]
+        self.shooting_frames_l = []
+        for frame in self.shooting_frames_r:
+            self.shooting_frames_l.append(pg.transform.flip(frame, True, False))
+        self.shootingIdle_frames_r = [pg.image.load('img/shooting1.png'), pg.image.load('img/shooting2.png'), pg.image.load('img/shooting3.png')]
+        self.shootingIdle_frames_l = []
+        for frame in self.shootingIdle_frames_r:
+            self.shootingIdle_frames_l.append(pg.transform.flip(frame, True, False))
 
     def jump_cut(self):
         if self.jumping:
@@ -45,17 +71,19 @@ class Player(pg.sprite.Sprite):
             self.vel.y = -PLAYER_JUMP
 
 
-
-
     def update(self):
         self.animate()
         self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
             self.acc.x = -PLAYER_ACC
+            self.left = True
+            self.right = False
 
         if keys[pg.K_d]:
             self.acc.x = PLAYER_ACC
+            self.right = True
+            self.left = False
 
         #apply friction
         self.acc.x += self.vel.x * PLAYER_FRICTION
@@ -74,7 +102,7 @@ class Player(pg.sprite.Sprite):
 
     def animate(self):
         now = pg.time.get_ticks()
-        if self.vel.x != 0:
+        if self.vel.x != 0 and not self.jumping:
             self.walking = True
         else:
             self.walking = False
@@ -85,31 +113,38 @@ class Player(pg.sprite.Sprite):
                 self.current_frame = (self.current_frame + 1) % len(self.walk_frames_l)
                 bottom = self.rect.bottom
                 if self.vel.x > 0:
-                    self.image = self.walk_frames_r[self.current_frame]
+                    self.image = self.shooting_frames_r[self.current_frame]
                 else:
-                    self.image = self.walk_frames_l[self.current_frame]
+                    self.image = self.shooting_frames_l[self.current_frame]
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
 
         # idle animation
-        if not self.jumping and not self.walking:
+        if not self.jumping and not self.walking and not self.shooting:
             if now - self.last_update > 120:
                 self.last_update = now
-                self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frames_r)
                 bottom = self.rect.bottom
-                self.image = self.standing_frames[self.current_frame]
+                if self.right:
+                    self.image = self.standing_frames_r[self.current_frame]
+                if self.left:
+                    self.image = self.standing_frames_l[self.current_frame]
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
 
         # jump animation
         if self.jumping:
-            if now - self.last_update > 100:
+            if now - self.last_update > 200:
                 self.last_update = now
-                self.current_frame = (self.current_frame + 1) % len(self.jumping_frames)
+                self.current_frame = (self.current_frame + 1) % len(self.jumping_frames_l)
                 bottom = self.rect.bottom
-                self.image = self.jumping_frames[self.current_frame]
+                if self.right:
+                    self.image = self.jumping_frames_r[self.current_frame]
+                if self.left:
+                    self.image = self.jumping_frames_l[self.current_frame]
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
+
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
@@ -118,3 +153,53 @@ class Platform(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+class Bullet_right(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = bullet
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = PROJECTILE_SPEED
+        self.player = Player(self)
+        self.player.shooting = True
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.left > WIDTH:
+            self.kill()
+
+class Bullet_left(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = bullet
+        self.image = pg.transform.flip(bullet, True, False)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = PROJECTILE_SPEED
+        self.player = Player(self)
+        self.player.shooting = True
+
+
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.right < 0:
+            self.kill()
+
+# class Bullet_up(pg.sprite.Sprite):
+#     def __init__(self, x, y):
+#         pg.sprite.Sprite.__init__(self)
+#         self.image = bullet
+#         self.image = pg.transform.rotate(bullet, 90)
+#         self.rect = self.image.get_rect()
+#         self.rect.x = x
+#         self.rect.y = y
+#         self.speed = PROJECTILE_SPEED
+#
+#
+#     def update(self):
+#         self.rect.y -= self.speed
+#         if self.rect.bottom < 0:
+#             self.kill()
