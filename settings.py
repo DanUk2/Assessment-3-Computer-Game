524b7a7b6313ae52a6e809e01ead606ae7f9d6e2
DanUk2 <43887736+DanUk2@users.noreply.github.com>
2019-05-07 00:44:01 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -14,12 +14,7 @@ PLAYER_JUMP = 15
 PROJECTILE_SPEED = 10
 
 #starting platforms (x, y, w, h)
-PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
-                (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
-                (125, HEIGHT - 350, 100, 20),
-                (350, 200, 100, 20),
-                (175, 100, 50, 20),
-                (150, 150, 100, 20)]
+PLATFORM_LIST = [(0, 500, 341, 50), (341, 500, 341, 50), (682, 500, 341, 50)]
 
 # define colours
 WHITE = (255, 255, 255)
