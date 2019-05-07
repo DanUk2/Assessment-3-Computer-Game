071d9b5263d11b4715dfad3b25337ba3b98b472b
DanUk2 <43887736+DanUk2@users.noreply.github.com>
2019-05-07 07:13:41 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,24 +1,29 @@
 #game options
 TITLE = "wonknodi"
-WIDTH = 1024
-HEIGHT = 576
+WIDTH = 960
+HEIGHT = 540
 FPS = 60
 
 #player properties
 
 PLAYER_HEALTH = 200
-PLAYER_ACC = 0.7
-PLAYER_FRICTION = -0.12
+PLAYER_ACC = 1
+PLAYER_FRICTION = -0.15
 PLAYER_GRAV = 0.8
-PLAYER_JUMP = 15
+PLAYER_JUMP = 17
 PLAYER_DAMAGE = 25
+
 
 PROJECTILE_SPEED = 10
 
 #starting platforms (x, y, w, h)
-PLATFORM_LIST = [(0, 500, 341, 50), (341, 500, 341, 50), (682, 500, 341, 50)]
+PLATFORM_LIST = [(0, 500, 341, 50), (341, 500, 341, 50), (682, 500, 341, 50),
+                (0, HEIGHT / 2, 200, 25),
+                (WIDTH - 200, HEIGHT / 2, 200, 25),
+                (WIDTH / 2 - 175, 380, 350, 25),
+                (WIDTH / 2 - 300, 150, 600, 25)]
 #crate spawn (x,y)
-CRATE_LIST = [(100, 100)]
+CRATE_LIST = [(150, 440), (WIDTH - 170, 440), (500, 320)]
 
 # define colours
 WHITE = (255, 255, 255)
