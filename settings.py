260c4c7a21996650a4930c1b8666f6a1772b7606
DanUk2 <43887736+DanUk2@users.noreply.github.com>
2019-05-07 02:34:34 +0100

    Add files via upload
    More player properties and a crate list

--- settings.py
+++ settings.py
@@ -6,15 +6,19 @@ FPS = 60
 
 #player properties
 
+PLAYER_HEALTH = 200
 PLAYER_ACC = 0.7
 PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.8
 PLAYER_JUMP = 15
+PLAYER_DAMAGE = 25
 
 PROJECTILE_SPEED = 10
 
 #starting platforms (x, y, w, h)
 PLATFORM_LIST = [(0, 500, 341, 50), (341, 500, 341, 50), (682, 500, 341, 50)]
+#crate spawn (x,y)
+CRATE_LIST = [(100, 100)]
 
 # define colours
 WHITE = (255, 255, 255)
